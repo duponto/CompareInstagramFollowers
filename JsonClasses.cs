69e0d3c0830f86b72fd22b0051c9eee1519d852e
Gabriel Dupont <82399843+duponto@users.noreply.github.com>
2025-07-28 21:53:32 -0300

    feat: solution

---
+++ JsonClasses.cs
@@ -0,0 +1,18 @@
+﻿namespace CompareFollowers;
+public class JsonFollowing
+{
+    public List<JsonFollower> relationships_following { get; set; }
+}
+public class JsonFollower
+{
+    public string title { get; set; }
+    public List<object> media_list_data { get; set; }
+    public List<StringListData> string_list_data { get; set; }
+
+}
+public class StringListData
+{
+    public string href { get; set; }
+    public string value { get; set; }
+    public long timestamp { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using CompareFollowers;
+using System.Text.Json;
+
+
+Console.WriteLine("Enter the directory path of the JSONs:");
+var directoryPath = Console.ReadLine();
+
+List<string> followers;
+using (StreamReader reader = new StreamReader(directoryPath + "/followers_1.json"))
+{
+    var json = JsonSerializer.Deserialize<List<JsonFollower>>(reader.ReadToEnd());
+    followers = json.SelectMany(f => f.string_list_data.Select(s => s.value)).ToList();
+    reader.Close();
+}
+
+List<string> following;
+using (StreamReader reader = new StreamReader(directoryPath + "/following.json"))
+{
+    var json = JsonSerializer.Deserialize<JsonFollowing>(reader.ReadToEnd());
+    following = json.relationships_following.SelectMany(f => f.string_list_data.Select(s => s.value)).ToList();
+    reader.Close();
+}
+
+List<string> followingNotInFollowers;
+
+followingNotInFollowers = following.Except(followers).ToList();
+
+Console.WriteLine(string.Join("\n", followingNotInFollowers));
+Console.ReadKey();
